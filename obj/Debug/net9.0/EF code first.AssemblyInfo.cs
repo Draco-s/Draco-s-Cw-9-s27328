1a082d61615abb8fb87df8bb36a4bd322d4c5026
Małgorzata Arndt <s27328@pjwstk.edu.pl>
2025-05-28 20:39:32 +0200

    "Poprawa umiejscowienia folderów"

--- obj/Debug/net9.0/EF code first.AssemblyInfo.cs
+++ obj/Debug/net9.0/EF code first.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EF code first")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+148efed67a5642cad02ba1fa8327a4a6aa0289f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("EF code first")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EF code first")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
