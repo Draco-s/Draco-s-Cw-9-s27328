b29abb95a26192aa79860d84c134c85d3a8287a0
Małgorzata Arndt <s27328@pjwstk.edu.pl>
2025-05-28 20:19:49 +0200

    "Initial commit"

---
+++ DTOs/AddPrescriptionDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EF_code_first.DTOs;
+
+public class AddPrescriptionDto
+{
+    [Required]
+    public PatientDto Patient { get; set; } = null!;
+
+    [Required]
+    public int IdDoctor { get; set; }
+
+    [Required]
+    public DateTime Date { get; set; }
+
+    [Required]
+    public DateTime DueDate { get; set; }
+
+    [Required]
+    [MaxLength(10, ErrorMessage = "Recepta może zawierać maksymalnie 10 leków.")]
+    public List<PrescriptionMedicamentDto> Medicaments { get; set; } = new();
+}
